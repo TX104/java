d8ed85ac9f37bd8ecf72294e1b3f762dddf82930
Administrator <Administrator@SC-201802201017>
2018-12-07 10:46:35 +0800

    test git commit

---
+++ src/com/tencent/day01/Hello.java
@@ -0,0 +1,13 @@
+package com.tencent.day01;
+
+/**
+ * 测试代码
+ * @author TangXin
+ * 2018年12月7日
+ */
+public class Hello {
+	
+	public static void main(String[] args) {
+		System.out.println("测试Hello World!");
+	}
+}
